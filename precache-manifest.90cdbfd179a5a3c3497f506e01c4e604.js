3e596a3beac3d639b21ca350f64f1bbe46ca3441
roso1nik <103901662+roso1nik@users.noreply.github.com>
2022-06-11 15:25:19 +0300

    release
    v1.0

---
+++ precache-manifest.90cdbfd179a5a3c3497f506e01c4e604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "250e1a12f3b94530f0a1",
+    "url": "/static/css/main.8f2bc5ab.chunk.css"
+  },
+  {
+    "revision": "250e1a12f3b94530f0a1",
+    "url": "/static/js/main.250e1a12.chunk.js"
+  },
+  {
+    "revision": "b2710742705637ba179e",
+    "url": "/static/js/1.b2710742.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7fac1d470e1bb0f3890300b0d4fbf4db",
+    "url": "/index.html"
+  }
+];
